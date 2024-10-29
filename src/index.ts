4056c86c50ab7bef725499e6fb2177795b47492a
Germano Benini <germano_benini@hotmail.com>
2024-10-28 22:09:34 -0300

    Add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
+
+app.use((req, res, next) => {
+  next();
+});
 
 app.get("/", (req, res) => {
-  res.send ('Hello, world!')  
-})
+  res.send("Hello, world!");
+});
 
 app.listen(3000, () => {
-  console.log('Server is listening on port 3000')
-})
+  console.log("Server is listening on port 3000");
+});
