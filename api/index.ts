fc6cdee14af619040fecc15e3f9fece4e59e8163
Germano Benini <germano_benini@hotmail.com>
2024-11-05 19:29:58 -0300

    Add cors

--- api/index.ts
+++ api/index.ts
@@ -3,9 +3,18 @@ import express from "express";
 import { routes } from "./routes";
 import swaggerUi from "swagger-ui-express";
 import { setupSwagger } from "./utils/utils";
+import cors from "cors";
+const allowedOrigins = ["http://localhost:6000"];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 const app = express();
+
+app.use(cors(options));
 app.use(express.json());
+
 app.use((req, res, next) => {
   next();
 });
@@ -15,7 +24,6 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(require("../openapi.json"), setupSwagger()),
 );
-
 app.use("/api", routes);
 
 app.listen(3000, () => {
