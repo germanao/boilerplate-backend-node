85dd72600631e41c37827fffe03a29dd62d13082
Germano Benini <germano_benini@hotmail.com>
2024-11-05 18:56:39 -0300

    :sparkles: Update swagger

---
+++ api/index.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import express from "express";
+import { routes } from "./routes";
+import swaggerUi from "swagger-ui-express";
+import { setupSwagger } from "./utils/utils";
+
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+  next();
+});
+
+app.use(
+  "/swagger-ui",
+  swaggerUi.serve,
+  swaggerUi.setup(require("../openapi.json"), setupSwagger()),
+);
+
+app.use("/api", routes);
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+module.exports = app;
